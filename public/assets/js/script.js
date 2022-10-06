fadf6d11e60a35272403d08f8f5ec41082531c15
Johanna Backlund Wass <johanna.bw1@gmail.com>
2022-10-06 14:14:21 +0200

    when you draw on one canvas it shows up on the other but not correct. it doesn't react when you stop drawing and start a new line on recieving canvas so it's just one long connected line instead of several single lines

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,21 @@
 const websocket = new WebSocket("ws://localhost:8080");
 
+const handleSocketOpen = (e) => {
+    console.log('Socket has been opened');
+    
+};
+
+const handleSocketMessage = (e) => {
+    const message = JSON.parse(e.data);
+    console.log("message", message);
+    if (message.type === "paint") {
+        paintToCanvas(message);
+    }
+};
+  
+websocket.onopen = handleSocketOpen;
+websocket.onmessage = handleSocketMessage;
+
 const canvas = document.getElementById('drawing-board');
 const toolbar = document.getElementById('toolbar');
 const ctx = canvas.getContext('2d');
@@ -15,33 +31,43 @@ let lineWidth = 5;
 let startX;
 let startY;
 
-toolbar.addEventListener('click', e => {
+toolbar.addEventListener('click', (e) => {
     if (e.target.id === 'clear') {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.reset();
+        // ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
 });
 
-toolbar.addEventListener('change', e => {
+toolbar.addEventListener('change', (e) => {
     if(e.target.id === 'stroke') {
-        ctx.strokeStyle = e.target.value;
+        let strokeStyleVariable = e.target.value;
+        ctx.strokeStyle = strokeStyleVariable;
     }
 
     if(e.target.id === 'lineWidth') {
-        lineWidth = e.target.value;
+        let lineWidthVariable = e.target.value;
+        lineWidth = lineWidthVariable;
     }
     
 });
 
-const draw = (e) => {
+const paint = (e) => {
     if(!isPainting) {
         return;
     }
 
     ctx.lineWidth = lineWidth;
     ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
 
     ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
     ctx.stroke();
+
+    // TODO send linewidth and color to paintObj
+    // console.log("Client X: ", e.clientX - canvasOffsetX, "Client Y: ", e.clientY);
+    let paintObj = {type: "paint", x: e.clientX - canvasOffsetX, y: e.clientY };
+    websocket.send(JSON.stringify(paintObj));
+    // websocket.send(e.clientX - canvasOffsetX, e.clientY);
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -50,10 +76,19 @@ canvas.addEventListener('mousedown', (e) => {
     startY = e.clientY;
 });
 
-canvas.addEventListener('mouseup', e => {
+canvas.addEventListener('mouseup', (e) => {
     isPainting = false;
     ctx.stroke();
     ctx.beginPath();
 });
 
-canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mousemove', paint);
+
+function paintToCanvas(message) {
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+    ctx.lineJoin = 'round';
+
+    ctx.lineTo(message.x, message.y);
+    ctx.stroke();
+}
--- server.js
+++ server.js
@@ -14,16 +14,14 @@ const wss = new WebSocketServer({ port: 8081 });
 
 server.on("upgrade", (req, socket, head) => {
     console.log("Upgrade event client: ", req.headers);
-    // start websocket
     wss.handleUpgrade(req, socket, head, (ws) => {
-        console.log("let user use websocket...");
+        // console.log("let user use websocket...");
 
         wss.emit("connection", ws, req);
     });
 });
 
 wss.on("connection", (ws) => {
-    console.log("New client connection from IP: ", ws._socket.remoteAddress);
     console.log("Number of connected clients: ", wss.clients.size);
 
     ws.on("close", () => {
@@ -32,7 +30,25 @@ wss.on("connection", (ws) => {
     });
 
     ws.on("message", (data) => {
-
+        // console.log("Message received: ", data); 
+       let obj = JSON.parse(data);
+       console.log(obj);
+    //    if (obj.type === "paint") {
+            wss.clients.forEach(client => {
+                if(client !== ws) {
+                    client.send(JSON.stringify(obj));
+                    
+                } 
+            });
+    //    }
+    //    if (obj.type === "comment") {
+    //     wss.clients.forEach(client => {
+    //         if(client !== ws) {
+    //             client.send(JSON.stringify(obj));
+                
+    //         } 
+    //     })
+    //    }
     });
 });
 
